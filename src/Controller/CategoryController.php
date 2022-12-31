42487956a0e39601ae4dc119f52982f6a8311d7d
Tristan Petit <tristan.petit01@gmail.com>
2022-12-31 18:25:57 +0100

    Ajout d'une pagination sur les listes des produits de chaque catégories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +13,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoryController extends AbstractController
 {
     #[Route('/{slug}', name: 'list')]
-    public function list(Category $category): Response
+    public function list(Category $category, ProductRepository $productRepository, Request $request): Response
     {
+        // numero de page dans l'url :
+        $page = $request->query->getInt('page', 1);
+
         //Liste des produits d'une catégorie
-        $product = $category->getProducts();
+        $product = $productRepository->findProductPaginated($page, $category->getSlug(), 9);
 
         return $this->render('category/list.html.twig', [
             'category' => $category,
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 /**
  * @extends ServiceEntityRepository<Product>
@@ -19,6 +20,42 @@ class ProductRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Product::class);
+    }
+
+    public function findProductPaginated(int $page, string $slug,  int $limit = 6): array
+    {
+        $limit = abs($limit);
+
+        $resultat = [];
+
+        $query = $this->getEntityManager()->createQueryBuilder()
+            ->select('c', 'p')
+            ->from('App\Entity\Product', 'p')
+            ->join('p.category', 'c')
+            ->where("c.slug = '$slug'")
+            ->setMaxResults($limit)
+            ->setFirstResult(($page * $limit) - $limit);
+
+        $paginator = new Paginator($query);
+        $data = $paginator->getQuery()->getResult();
+
+        // vérifie si on a des données
+        if (empty($data)) {
+            return $resultat;
+        }
+
+        // calcule le nombre de pages
+        $pages = ceil(count($paginator) / $limit);
+
+        // rempli le tableau de résultat
+        $resultat = [
+            'data' => $data,
+            'pages' => $pages,
+            'page' => $page,
+            'limit' => $limit,
+        ];
+
+        return $resultat;
     }
 
     public function save(Product $entity, bool $flush = false): void
@@ -39,28 +76,28 @@ class ProductRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Product[] Returns an array of Product objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Product[] Returns an array of Product objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('p.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?Product
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?Product
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
